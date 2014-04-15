80ed9a2d1f890e60c928152fd9f419d4c2b2cfd8
YongChingTee <tyc_10@msn.com>
2014-04-15 02:41:05 -0400

    ready to test

--- config/twilio-helper.js
+++ config/twilio-helper.js
@@ -11,7 +11,7 @@ var client = new twilio.RestClient('AC0b9e9c8a50f9acb63de3571d79f42a91', '833aaa
 exports.sendGuardianRequest = function (guardianContact, requesterName, requestStartDate, requestEndDate){
         var guardianName = guardianContact.name;
         var requestStartTime = moment(requestStartDate).format("h A");
-        var requestEndTime = moment(requesEndDate).format("hh A");
+        var requestEndTime = moment(requestEndDate).format("hh A");
 
         // 'Hello '+guardianName+'! Would you like to be'+requesterName+'\'s Guardian from '+requestStartTime+' onwards, please respond with a YES or NO?';
         var messageBody = 'Hello '+guardianName;
@@ -24,6 +24,31 @@ exports.sendGuardianRequest = function (guardianContact, requesterName, requestS
         to: guardianContact.phone,
         from:'+16788827697',
         body: messageBody
+    }, function(error, message) {
+        // The HTTP request to Twilio will run asynchronously. This callback
+        // function will be called when a response is received from Twilio
+        // The "error" variable will contain error information, if any.
+        // If the request was successful, this value will be "falsy"
+        if (!error) {
+            // The second argument to the callback will contain the information
+            // sent back by Twilio for the request. In this case, it is the
+            // information about the text messsage you just sent:
+            console.log('Success! The SID for this SMS message is:');
+            //console.log(message);
+
+        } else {
+            console.log('Oops! There was an error.');
+        }
+    });
+}
+
+exports.sendMessage = function (message, guardianContact){
+        
+        //console.log("Twilio Helper with "+guardianContact.phone+" "+email);
+        client.sendSms({
+        to: guardianContact.phone,
+        from:'+16788827697',
+        body: message
     }, function(error, message) {
         // The HTTP request to Twilio will run asynchronously. This callback
         // function will be called when a response is received from Twilio
